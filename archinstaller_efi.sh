4e163cd440a57919b742e14dbc4a6d21b0a1d0f5
Daniel Field <danielfield3515@protonmail.com>
2022-02-21 19:52:44 +1000

    Added EFI install script

---
+++ archinstaller_efi.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+timedatectl set-ntp true
+
+modprobe dm-crypt
+modprobe dm-mod
+echo "This script will now list the disks, please remember which one you want to partition."
+read -r -s -p $'Press enter to continue...'
+fdisk -l
+echo "Which disk would you like to partition? (e.g. /dev/sda)"
+read diskname
+echo "You will now be prompted to create your partitions."
+
+echo "For EFI mode, create these partitions:"
+echo "EFI (recommended 256MB)"
+echo "boot (recommended 512MB)"
+echo "root (remaining space on drive)"
+
+read -r -s -p $'Press enter to continue...'
+
+cfdisk $diskname
+
+echo "Finished creating partitions!"
+echo "listing disks again..."
+fdisk -l
+echo "What is the EFI partition (e.g. /dev/sda1)? "
+read efipart
+echo "What is the boot partition? (e.g. /dev/sda2)? "
+read bootpart
+echo "What is the root partition? (e.g. /dev/sda3)? "
+read rootpart
+echo "EFI partition = $efipart, boot partition = $bootpart, root partition = $rootpart"
+
+echo "Setting up encryption with luksFormat..."
+cryptsetup luksFormat -v -s 512 -h sha512 $rootpart
+cryptsetup open $rootpart luks_root
+
+mkfs.vfat -n "EFI" $efipart
+mkfs.ext4 -L boot $bootpart
+mkfs.ext4 -L root /dev/mapper/luks_root
+
+mount /dev/mapper/luks_root /mnt
+mkdir /mnt/boot
+mount $bootpart /mnt/boot
+mkdir /mnt/boot/efi
+mount $efipart /mnt/boot/efi
+
+echo "How big would you like your swap file (in MB)? "
+read swapsize
+dd if=/dev/zero of=/mnt/swap bs=1M count=$swapsize
+mkswap /mnt/swap
+swapon /mnt/swap
+chmod 0600 /mnt/swap
+
+echo "Installing packages..."
+echo "Which kernel would you like? (e.g. linux, linux-lts, linux-zen, etc.) "
+read kernelpackage
+pacman -Syy
+pacstrap /mnt base base-devel efibootmgr grub $kernelpackage linux-firmware networkmanager sudo vi vim bash-completion nano wget ufw git pulseaudio pavucontrol network-manager-applet archlinux-keyring
+arch-chroot /mnt systemctl enable NetworkManager.service
+arch-chroot /mnt systemctl enable ufw
+arch-chroot /mnt ufw enable
+
+echo "generating fstab..."
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "Setting timezone..."
+arch-chroot /mnt /bin/bash <<"EOT"
+ln -sf /usr/share/zoneinfo/Australia/Brisbane /etc/localtime
+hwclock --systohc
+EOT
+
+echo "Setting locale to Australia..."
+arch-chroot /mnt /bin/bash <<"EOT"
+echo -e "en_AU.UTF-8 UTF-8\nen_AU ISO-8859-1" > /etc/locale.gen
+locale-gen
+echo 'LANG=en_AU.UTF-8' > /etc/locale.conf
+echo 'KEYMAP=US' > /etc/vconsole.conf
+EOT
+
+
+echo "What do you want your hostname to be?"
+read myhostname
+arch-chroot /mnt /bin/bash -c "echo $myhostname > /etc/hostname"
+arch-chroot /mnt rm /etc/hosts
+arch-chroot /mnt touch /etc/hosts
+arch-chroot /mnt /bin/bash -c "echo -e \"127.0.0.1 localhost\n::1 localhost\n127.0.1.1 $myhostname.localdomain $myhostname\" > /etc/hosts"
+
+echo "Setting up grub to work with LUKS..."
+arch-chroot /mnt sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"cryptdevice=${rootpart//\//\\/}:luks_root\"/" /etc/default/grub
+
+echo "Generating initramfs..."
+#echo "REMEMBER: add encrypt to this line like so:"
+#echo "HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)"
+#read -r -s -p $'Press enter to continue...'
+arch-chroot /mnt sed -i "s/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/" /etc/mkinitcpio.conf
+arch-chroot /mnt mkinitcpio -P
+
+echo "Set the root password:"
+arch-chroot /mnt passwd
+
+echo "Setting up grub..."
+arch-chroot /mnt grub-install --boot-directory=/boot --efi-directory=/boot/efi $bootpart
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-mkconfig -o /boot/efi/EFI/arch/grub.cfg
+
+echo "Making a new user..."
+echo "Enter a new username:"
+read myusername
+arch-chroot /mnt useradd -m -G wheel $myusername
+arch-chroot /mnt passwd $myusername
+
+arch-chroot /mnt /bin/bash <<"EOT"
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+EOT
+arch-chroot /mnt nano /etc/sudoers
+
+echo "Which CPU microcode would you like? (type amd-ucode or intel-ucode)"
+read ucodepackage
+pacstrap /mnt $ucodepackage
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Installing yay and it's dependencies..."
+pacstrap /mnt go
+arch-chroot /mnt /bin/bash <<"EOT"
+useradd -m tempuser
+su tempuser
+cd ~
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg
+exit
+pacman --noconfirm -U /home/tempuser/yay-git/*.zst
+userdel tempuser
+rm -rf /home/tempuser
+EOT
+
+pacman --noconfirm -S archlinux-keyring
+
+./desktop-env-install.sh
+
+echo -e "\n\nArch has been installed, reboot when you are ready. Have fun!"
---
+++ desktop-env-install.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+read -p "Would you like to install xorg (y/n)?" choice
+case "$choice" in 
+	y|Y) 
+			echo "Installing xorg..."
+			pacstrap /mnt xorg xorg-server xorg-apps
+	;;
+	n|N) 
+			echo "Skipped xorg installation."
+	;;
+	*) 
+			echo "invalid. Skipped."
+	;;
+esac
+
+echo -e "\nChoose a video driver:"
+echo "1. NVIDIA"
+echo "2. AMD"
+echo "3. VMWare"
+echo "4. Skip video driver installation"
+
+read videodriver
+
+case "$videodriver" in
+        "1")
+                pacstrap -i /mnt nvidia nvidia-utils
+        ;;
+        "2")
+                pacstrap -i /mnt xf86-video-amdgpu
+        ;;
+        "3")
+                pacstrap -i /mnt xf86-video-vmware spice-vdagent
+        ;;
+		"4")
+                echo "Skipped video driver installation."
+        ;;
+		*)
+                echo "Invalid. Skipped video driver installation."
+        ;;
+esac
+
+echo -e "\nChoose a desktop environment:"
+echo "1. XFCE4"
+echo "2. GNOME"
+echo "3. KDE Plasma"
+echo "4. Budgie"
+echo "5. Cinnamon"
+echo "6. LXDE"
+echo "7. No desktop environment (skips this step)"
+
+read desktopenv
+
+case "$desktopenv" in
+        "1")
+                echo "Installing xfce4..."
+                pacstrap -i /mnt xfce4 xfce4-goodies
+                pacstrap -i /mnt lightdm lightdm-gtk-greeter
+                arch-chroot /mnt systemctl enable lightdm
+                echo "Done."
+        ;;
+        "2")
+                echo "Installing gnome..."
+				pacstrap -i /mnt gnome gnome-extra gnome-tweaks firefox vlc
+				arch-chroot /mnt systemctl enable gdm.service
+                echo "Done."
+        ;;
+        "3")
+                echo "Installing KDE Plasma..."
+				pacstrap -i /mnt plasma plasma-wayland-session kde-applications sddm
+				arch-chroot /mnt systemctl enable sddm.service
+                echo "Done."
+        ;;
+        "4")
+                echo "Installing Budgie..."
+				pacstrap -i /mnt budgie-desktop sddm gnome-terminal firefox vlc gnome-control-center nomacs
+				arch-chroot /mnt systemctl enable sddm.service
+                echo "Done."
+        ;;
+        "5")
+                echo "Installing Cinnamon..."
+				pacstrap -i /mnt cinnamon gnome-terminal
+				pacstrap -i /mnt lightdm lightdm-gtk-greeter
+				arch-chroot /mnt systemctl enable lightdm
+                echo "Done."
+        ;;
+        "6")
+                echo "Installing LXDE..."
+                pacstrap -i /mnt lxde lxdm leafpad network-manager-applet opera
+                arch-chroot /mnt systemctl enable lxdm.service
+                echo "Done."
+        ;;
+        "7")
+                echo "Skipped desktop environment installation."
+        ;;
+        *)
+                echo "Invalid option."
+                echo "Skipped desktop environment installation."
+        ;;
+esac
