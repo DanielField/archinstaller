cc297e2492b894d20b32ad2df888b911f89077d1
Daniel Field <danielfield3515@protonmail.com>
2022-03-13 21:54:00 +1000

    All prompts are now asked at beginning

--- archinstaller_efi.sh
+++ archinstaller_efi.sh
@@ -8,9 +8,33 @@ modprobe dm-mod
 fdisk -l
 read -p "Which device do you want to partition? " TGTDEV
 read -p "Enter a hostname for your system: " myhostname
+read -p "Which kernel(s) would you like? (if multiple, separate with spaces. e.g. linux linux-lts) " kernelpackage
+read -p "How big would you like your swap file (in MB)? " swapsize
 read -s -p "Create a root password: " rootpassword
-read -p "What would you like your username to be? " myusername
+read -p "\nWhat would you like your username to be? " myusername
 read -s -p "Create a password for $myusername: " mypassword
+echo "\nWhich CPU microcode would you like?"
+echo "1. intel-ucode"
+echo "2. amd-ucode"
+echo "3. Skip"
+read ucodepackage
+read -p "Would you like to install xorg (y/n)?" xorgchoice
+echo -e "\nChoose a video driver:"
+echo "1. NVIDIA"
+echo "2. AMD"
+echo "3. Skip video driver installation"
+read videodriver
+echo -e "\nChoose a desktop environment:"
+echo "1. Xfce4"
+echo "2. GNOME"
+echo "3. KDE Plasma"
+echo "4. Budgie"
+echo "5. Cinnamon"
+echo "6. LXQT"
+echo "7. MATE"
+echo "8. No desktop environment (skips this step)"
+read desktopenv
+read -p "Are you running QEMU/KVM and want to install the Spice agent (y/n)?" spice
 
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
   g # create GPT partition table
@@ -61,16 +85,12 @@ mount $bootpart /mnt/boot
 mkdir /mnt/boot/efi
 mount $efipart /mnt/boot/efi
 
-echo "How big would you like your swap file (in MB)? "
-read swapsize
 dd if=/dev/zero of=/mnt/swap bs=1M count=$swapsize
 mkswap /mnt/swap
 swapon /mnt/swap
 chmod 0600 /mnt/swap
 
 echo "Installing packages..."
-echo "Which kernel would you like? (e.g. linux, linux-lts, linux-zen, etc.) "
-read kernelpackage
 pacman -Syy
 pacstrap /mnt base base-devel efibootmgr grub $kernelpackage linux-firmware networkmanager sudo vi vim bash-completion nano wget ufw git pulseaudio pavucontrol network-manager-applet archlinux-keyring
 arch-chroot /mnt systemctl enable NetworkManager.service
@@ -120,11 +140,6 @@ arch-chroot /mnt grub-mkconfig -o /boot/efi/EFI/arch/grub.cfg
 
 echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
 
-echo "Which CPU microcode would you like?"
-echo "1. intel-ucode"
-echo "2. amd-ucode"
-echo "3. Skip"
-read ucodepackage
 case "$ucodepackage" in
         "1")
                 pacstrap /mnt intel-ucode
@@ -164,6 +179,100 @@ sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 5/" /mnt/etc/pacman.conf
 
 pacman --noconfirm -S archlinux-keyring
 
-./desktop-env-install.sh
+case "$xorgchoice" in 
+	y|Y) 
+			echo "Installing xorg..."
+			pacstrap /mnt xorg xorg-server xorg-apps
+	;;
+	n|N) 
+			echo "Skipped xorg installation."
+	;;
+	*) 
+			echo "invalid. Skipped."
+	;;
+esac
+
+case "$videodriver" in
+        "1")
+                pacstrap /mnt nvidia nvidia-utils
+        ;;
+        "2")
+                pacstrap /mnt xf86-video-amdgpu
+        ;;
+		"3")
+                echo "Skipped video driver installation."
+        ;;
+		*)
+                echo "Invalid. Skipped video driver installation."
+        ;;
+esac
+
+case "$desktopenv" in
+        "1")
+                echo "Installing xfce4..."
+                pacstrap /mnt xfce4 xfce4-goodies
+                pacstrap /mnt lightdm lightdm-gtk-greeter
+                arch-chroot /mnt systemctl enable lightdm
+                echo "Done."
+        ;;
+        "2")
+                echo "Installing gnome..."
+				pacstrap /mnt gnome gnome-extra gnome-tweaks
+				arch-chroot /mnt systemctl enable gdm.service
+                echo "Done."
+        ;;
+        "3")
+                echo "Installing KDE Plasma..."
+				pacstrap /mnt plasma plasma-wayland-session kde-applications sddm
+				arch-chroot /mnt systemctl enable sddm.service
+                echo "Done."
+        ;;
+        "4")
+                echo "Installing Budgie..."
+				pacstrap /mnt budgie-desktop sddm gnome-terminal gnome-control-center nomacs
+				arch-chroot /mnt systemctl enable sddm.service
+                echo "Done."
+        ;;
+        "5")
+                echo "Installing Cinnamon..."
+				pacstrap /mnt cinnamon gnome-terminal
+				pacstrap /mnt lightdm lightdm-gtk-greeter
+				arch-chroot /mnt systemctl enable lightdm
+                echo "Done."
+        ;;
+        "6")
+                echo "Installing LXQT..."
+                pacstrap /mnt lxqt sddm oxygen-icons lxqt-panel network-manager-applet
+                arch-chroot /mnt systemctl enable sddm.service
+                echo "Done."
+        ;;
+		"7")
+                echo "Installing MATE..."
+                pacstrap /mnt mate mate-extra
+				pacstrap /mnt lightdm lightdm-gtk-greeter
+                arch-chroot /mnt systemctl enable lightdm
+                echo "Done."
+        ;;
+		"8")
+                echo "Skipped desktop environment installation."
+        ;;
+        *)
+                echo "Invalid option."
+                echo "Skipped desktop environment installation."
+        ;;
+esac
+
+case "$spice" in 
+	y|Y) 
+			echo "Installing spice-vdagent..."
+			pacstrap /mnt spice-vdagent
+	;;
+	n|N) 
+			echo "Skipped spice installation."
+	;;
+	*) 
+			echo "invalid. Skipped."
+	;;
+esac
 
 echo -e "\n\nArch has been installed, reboot when you are ready. Have fun!"
